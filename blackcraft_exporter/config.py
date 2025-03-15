1d3c21471cecfc7aa47e567e206eb48d0ec9658f
Fallen_Breath <nellaforax@outlook.com>
2025-03-16 03:09:49 +0800

    CLI add `--version` flag to show version and exit

--- blackcraft_exporter/config.py
+++ blackcraft_exporter/config.py
@@ -1,5 +1,14 @@
+import sys
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict, CliApp, CliImplicitFlag
+from typing_extensions import NoReturn
+
+
+def _show_version_and_exit() -> NoReturn:
+	from blackcraft_exporter import __version__
+	print(f'BlackCraft Exporter v{__version__}')
+	sys.exit(0)
 
 
 class Config(BaseSettings):
@@ -8,6 +17,7 @@ class Config(BaseSettings):
 		env_prefix='BCE_',
 		cli_kebab_case=True,
 	)
+	version: CliImplicitFlag[bool] = Field(default=False, description='Show version and exit')
 
 	host: str = Field(default='0.0.0.0', description='Host to listen on')
 	port: int = Field(default=9165, description='Port to listen on')
@@ -16,6 +26,10 @@ class Config(BaseSettings):
 	debug: CliImplicitFlag[bool] = Field(default=False, description='Enable debug logging')
 
 	dev_mode: CliImplicitFlag[bool] = Field(default=False, description='Enable development mode. Not for production use')
+
+	def cli_cmd(self) -> None:
+		if self.version:
+			_show_version_and_exit()
 
 __config = Config()
 
@@ -26,5 +40,5 @@ def get_config() -> Config:
 
 def load_config_from_argv() -> Config:
 	global __config
-	__config = CliApp.run(Config)
+	__config = CliApp.run(Config, cli_cmd_method_name='cli_cmd')
 	return get_config()
